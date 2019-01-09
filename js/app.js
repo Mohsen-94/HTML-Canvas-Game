b80209fcb303f6fa8957d5d1bcd764be1a9bb1a8
Mohsen-94 <34848846+Mohsen-94@users.noreply.github.com>
2019-01-09 19:07:56 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -125,7 +125,7 @@ function randomSpeed() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
